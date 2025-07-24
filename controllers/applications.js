0cc741df2a11725ecf25f9d9604e0f785f200665
morgantstewart <morgan@paysigil.com>
2025-07-24 16:24:09 -0400

    pushing updates to edit functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -84,6 +84,22 @@ router.delete('/:applicationId', async (req, res) => {
 });
 
 
+// controllers/applications.js
+
+router.get('/:applicationId/edit', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const application = currentUser.applications.id(req.params.applicationId);
+    res.render('applications/edit.ejs', {
+      application: application,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+
 
 
 
